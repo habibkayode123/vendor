62bee24a93996b56b6e5565734216e57a7588b07
Habib Popoola <habibkayodenew@gmail.com>
2021-03-08 13:38:52 +0100

    useId field added to singleLogs component

--- src/views/PurchaseRequests/Logs/SingleLog.js
+++ src/views/PurchaseRequests/Logs/SingleLog.js
@@ -1,109 +1,116 @@
-import React, {useEffect, useState} from 'react';
-import axios from '../../../axios';
+import React, { useEffect, useState } from "react";
+import axios from "../../../axios";
 import {
-	Card,
-	Container,
-	Row,
-    Col,
-    Table,
-    Button,
-    Modal,
-    Form,
-    Accordion
+  Card,
+  Container,
+  Row,
+  Col,
+  Table,
+  Button,
+  Modal,
+  Form,
+  Accordion,
 } from "react-bootstrap";
-import {numberWithCommas} from '../../../helpers';
-import auth from 'auth/auth-helper';
-import {toast } from 'react-toastify';
+import { numberWithCommas } from "../../../helpers";
+import auth from "auth/auth-helper";
+import { toast } from "react-toastify";
 
 function SingleLog(props) {
-    // console.log("props...",props)
-    const init = {
-        vendorId: '',
-        name: '',
-        quantity: '',
-        amount: ''
+  // console.log("props...",props)
+  const init = {
+    vendorId: "",
+    name: "",
+    quantity: "",
+    amount: "",
+  };
+  const [log, setLog] = useState({ amount: 0 });
+  const [vendors, setVendors] = useState([]);
+  const [values, setValues] = useState(init);
+  const [items, setItems] = useState([]);
+  const [expenseTypeId, setExpenseTypeId] = useState("");
+
+  const handleOnChange = ({ target }) => {
+    console.log("Targets", target);
+    setValues({ ...values, [target.name]: target.value });
+  };
+
+  const fetchLog = () => {
+    const uuid = props.match.params.uuid;
+    axios.get(`/v1/log/${uuid}`).then((res) => {
+      console.log("singleLog", res.data.data);
+      setExpenseTypeId(res.data.data.data.expenseTypeId);
+      setLog(res.data.data.data);
+    });
+  };
+
+  const fetchVendors = () => {
+    axios.get("/v1/vendor").then((res) => {
+      setVendors(res.data.data);
+    });
+  };
+
+  const handleSubmit = () => {
+    console.log("datalogs", items);
+    auth;
+    const data = items.map((item) => {
+      const newItems = [
+        {
+          quantity: item.quantity,
+          name: item.name,
+          amount: item.amount,
+        },
+      ];
+
+      return {
+        vendorId: item.vendorId,
+        items: newItems,
+      };
+    });
+
+    const payload = {
+      total: items.reduce((a, c) => a + parseFloat(c.amount), 0).toString(),
+      logId: log.id,
+      departmentId: log.departmentId,
+      expenseTypeId: log.expenseTypeId,
+      orders: data,
+      requestedBy: auth.isAuthenticated().user.email.split("@")[0],
+      userId: auth.isAuthenticated().user.id,
     };
-    const [log, setLog] = useState({amount: 0});
-    const [vendors, setVendors] = useState([]);
-    const [values, setValues] = useState(init);
-    const [items, setItems] = useState([]);
-    const [expenseTypeId,setExpenseTypeId]= useState('');
 
-    const handleOnChange = ({target}) => {
-        console.log("Targets",target);
-        setValues({...values, [target.name]: target.value});
-    }
-
-    const fetchLog = () => {
-        const uuid = props.match.params.uuid;
-        axios.get(`/v1/log/${uuid}`).then(res => {
-            console.log("singleLog",res.data.data)
-            setExpenseTypeId(res.data.data.data.expenseTypeId);
-            setLog(res.data.data.data);
-        });
-    }
-
-    const fetchVendors = () => {
-        axios.get('/v1/vendor').then(res => {
-            setVendors(res.data.data);
-        })
-    }
-
-    const handleSubmit = () => {
-        console.log("datalogs", items);
-        const data = items.map(item => {
-            const newItems = [{
-                quantity: item.quantity,
-                name: item.name,
-                amount: item.amount
-            }];
-
-            return {
-                vendorId: item.vendorId,
-                items: newItems
-            }
-        });
-
-        const payload = {
-            total: items.reduce((a, c) => a + parseFloat(c.amount), 0).toString(),
-            logId: log.id,
-            departmentId: log.departmentId,
-            expenseTypeId: log.expenseTypeId,
-            orders: data,
-            requestedBy: auth.isAuthenticated().user.email.split('@')[0]
-        };
-
-        // console.log("propsBudget",props)
-        props.setBudgets(payload.total,expenseTypeId)
-        console.log("Payloads...",payload)
-        axios.post('/v1/request', payload).then(res => {
-            toast.success(res.data.message);
-            setItems([]);
-            setValues(init);
-        }).catch(err => {
-            console.log("Payload From singleLog",payload,err.response)
-        })
-    }
-
-    const handleAddItem = () => {
-        setItems([...items, values]);
+    // console.log("propsBudget",props)
+    props.setBudgets(payload.total, expenseTypeId);
+    console.log("Payloads...", payload);
+    axios
+      .post("/v1/request", payload)
+      .then((res) => {
+        toast.success(res.data.message);
+        setItems([]);
         setValues(init);
-    }
+      })
+      .catch((err) => {
+        console.log("Payload From singleLog", payload, err.response);
+      });
+  };
 
-    useEffect(() => {
-        fetchLog();
-        fetchVendors();
-    }, [])
+  const handleAddItem = () => {
+    setItems([...items, values]);
+    setValues(init);
+  };
 
-    return (
-        <Container fluid>
-            <Row>
-                <Col md="12">
-                    <Card>
-                        <Card.Header>
-                            <Card.Title className="d-flex justify-content-between">
-                                <h4>Purchase Request Log</h4>
-                                {/* <div className="">
+  useEffect(() => {
+    fetchLog();
+    fetchVendors();
+  }, []);
+
+  return (
+    <Container fluid>
+      <Row>
+        <Col md="12">
+          <Card>
+            <Card.Header>
+              <Card.Title className="d-flex justify-content-between">
+                <h4>Purchase Request Log</h4>
+                {/* <div className="">
                                     {!request.reviewDate && isActionAllowed('review') && (<Button size="sm" onClick={() => handleShow('Review')} >Review</Button>)}
                                     {
                                         isActionAllowed('approve') &&
@@ -130,67 +137,69 @@ function SingleLog(props) {
                                         )
                                     }
                                 </div> */}
-                            </Card.Title>
-                        </Card.Header>
-                        <Card.Body>
-                            <Row>
-                                <Col md="3">
-                                    <Card>
-                                        <Card.Header>
-                                            <Card.Title as="h6">Narration:</Card.Title>
-                                        </Card.Header>
-                                        <Card.Body>
-                                            <Card.Text>{log.narration}</Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-                                <Col md="3">
-                                    <Card>
-                                        <Card.Header>
-                                            <Card.Title as="h6">Amount:</Card.Title>
-                                        </Card.Header>
-                                        <Card.Body>
-                                            <Card.Text>{numberWithCommas(log.amount)}</Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-                                <Col md="3">
-                                    <Card>
-                                        <Card.Header>
-                                            <Card.Title as="h6">Date:</Card.Title>
-                                        </Card.Header>
-                                        <Card.Body>
-                                            <Card.Text>{new Date(log.createdAt).toLocaleDateString()}</Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-                            </Row>
+              </Card.Title>
+            </Card.Header>
+            <Card.Body>
+              <Row>
+                <Col md="3">
+                  <Card>
+                    <Card.Header>
+                      <Card.Title as="h6">Narration:</Card.Title>
+                    </Card.Header>
+                    <Card.Body>
+                      <Card.Text>{log.narration}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col md="3">
+                  <Card>
+                    <Card.Header>
+                      <Card.Title as="h6">Amount:</Card.Title>
+                    </Card.Header>
+                    <Card.Body>
+                      <Card.Text>{numberWithCommas(log.amount)}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col md="3">
+                  <Card>
+                    <Card.Header>
+                      <Card.Title as="h6">Date:</Card.Title>
+                    </Card.Header>
+                    <Card.Body>
+                      <Card.Text>
+                        {new Date(log.createdAt).toLocaleDateString()}
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
 
-                        
-                            <Row>
-                                <Col md="12">
-                                    <h5>Raise Request</h5>
-                                </Col>
-                                <Col md="4">
-                                    <Form>
-                                        <Form.Group controlId="vendorId">
-                                            <Form.Label>Vendor</Form.Label>
-                                            <Form.Control as="select"
-                                                name="vendorId"
-                                                value={values.vendorId}
-                                                onChange={handleOnChange}
-                                            >
-                                                <option>Choose</option>
-                                                {vendors.map((e, key) => {
-                                                    return (
-                                                        <option value={e.id} key={key}>
-                                                            {e.title}
-                                                        </option>
-                                                    );
-                                                })}
-                                            </Form.Control>
-                                        </Form.Group>
-                                        {/* <Form.Group controlId="narration">
+              <Row>
+                <Col md="12">
+                  <h5>Raise Request</h5>
+                </Col>
+                <Col md="4">
+                  <Form>
+                    <Form.Group controlId="vendorId">
+                      <Form.Label>Vendor</Form.Label>
+                      <Form.Control
+                        as="select"
+                        name="vendorId"
+                        value={values.vendorId}
+                        onChange={handleOnChange}
+                      >
+                        <option>Choose</option>
+                        {vendors.map((e, key) => {
+                          return (
+                            <option value={e.id} key={key}>
+                              {e.title}
+                            </option>
+                          );
+                        })}
+                      </Form.Control>
+                    </Form.Group>
+                    {/* <Form.Group controlId="narration">
                                             <Form.Label>Vendor</Form.Label>
                                             <Form.Control
                                                 as="textarea"
@@ -200,57 +209,60 @@ function SingleLog(props) {
                                                 onChange={handleInputChange}
                                             />
                                         </Form.Group> */}
-                                        <Form.Group controlId="item">
-                                            <Form.Label>Item</Form.Label>
-                                            <Form.Control
-                                                type="text"
-                                                placeholder="Product Name"
-                                                name="name"
-                                                value={values.name}
-                                                onChange={handleOnChange}
-                                            />
-                                        </Form.Group>
-                                        <Form.Group controlId="quantity">
-                                            <Form.Label>Quantity</Form.Label>
-                                            <Form.Control
-                                                type="number"
-                                                placeholder="Qty"
-                                                name="quantity"
-                                                value={values.quantity}
-                                                onChange={handleOnChange}
-                                            />
-                                        </Form.Group>
-                                        <Form.Group controlId="amount">
-                                            <Form.Label>Amount</Form.Label>
-                                            <Form.Control
-                                                type="number"
-                                                placeholder="Amount"
-                                                name="amount"
-                                                value={values.amount}
-                                                onChange={handleOnChange}
-                                            />
-                                        </Form.Group>
-                                        <Button onClick={handleAddItem}>Add</Button>
-                                    </Form>
-                                </Col>
-                                {items.length > 0 && <Col md="8">
-                                    <Table striped bordered hover>
-                                        <thead>
-                                            <tr>
-                                                <th>Vendor</th>
-                                                <th>Item Name</th>
-                                                <th>Qty</th>
-                                                <th>Amount</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {items.map((item, index) => (
-                                                <tr key={index}>
-                                                    <td>{vendors.find(v => v.id == item.vendorId).title}</td>
-                                                    <td>{item.name}</td>
-                                                    <td>{item.quantity}</td>
-                                                    <td>{numberWithCommas(item.amount)}</td>
-                                                    {/* <td>
+                    <Form.Group controlId="item">
+                      <Form.Label>Item</Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Product Name"
+                        name="name"
+                        value={values.name}
+                        onChange={handleOnChange}
+                      />
+                    </Form.Group>
+                    <Form.Group controlId="quantity">
+                      <Form.Label>Quantity</Form.Label>
+                      <Form.Control
+                        type="number"
+                        placeholder="Qty"
+                        name="quantity"
+                        value={values.quantity}
+                        onChange={handleOnChange}
+                      />
+                    </Form.Group>
+                    <Form.Group controlId="amount">
+                      <Form.Label>Amount</Form.Label>
+                      <Form.Control
+                        type="number"
+                        placeholder="Amount"
+                        name="amount"
+                        value={values.amount}
+                        onChange={handleOnChange}
+                      />
+                    </Form.Group>
+                    <Button onClick={handleAddItem}>Add</Button>
+                  </Form>
+                </Col>
+                {items.length > 0 && (
+                  <Col md="8">
+                    <Table striped bordered hover>
+                      <thead>
+                        <tr>
+                          <th>Vendor</th>
+                          <th>Item Name</th>
+                          <th>Qty</th>
+                          <th>Amount</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {items.map((item, index) => (
+                          <tr key={index}>
+                            <td>
+                              {vendors.find((v) => v.id == item.vendorId).title}
+                            </td>
+                            <td>{item.name}</td>
+                            <td>{item.quantity}</td>
+                            <td>{numberWithCommas(item.amount)}</td>
+                            {/* <td>
                                                         <Button
                                                             className="mr-1"
                                                             variant="danger"
@@ -266,20 +278,20 @@ function SingleLog(props) {
                                                             Edit
                                                         </Button>
                                                     </td> */}
-                                                </tr>
-                                            ))}
-                                        </tbody>
-                                    </Table>
-                                    <Button onClick={handleSubmit}>Raise Request</Button>
-                                </Col>}
-                            </Row>
-                            
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
-    );
+                          </tr>
+                        ))}
+                      </tbody>
+                    </Table>
+                    <Button onClick={handleSubmit}>Raise Request</Button>
+                  </Col>
+                )}
+              </Row>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default SingleLog;
